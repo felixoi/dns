66edb093ab77c065018f6f75407c8bc91a138638
Felix Kleinekathoefer <felix@felixoi.com>
2022-12-16 09:17:40 +0100

    First try of sharing records between zones

---
+++ NamedFileYamlProvider.py
@@ -0,0 +1,13 @@
+from os.path import join
+
+from octodns.provider.yaml import YamlProvider
+
+
+class NamedFileYamlProvider(YamlProvider):
+
+    def __init__(self, id, filename, *args, **kwargs):
+        super().__init__(id, *args, **kwargs)
+        self.filename = filename
+
+    def get_filenames(self, zone):
+        return join(self.directory, f'{self.filename}.yaml')
